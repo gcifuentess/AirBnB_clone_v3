1c237e4358a100bf02b0e9f84612bbf34b068125
xioperez01 <angie.xioperez@gmail.com>
2021-01-27 16:54:42 +0000

    adding error handles in app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """API module for HBNB proyect
 """
-from flask import Flask, Blueprint
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -14,6 +14,13 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown_db(exception):
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Page Not found"""
+    return make_response(jsonify({"error": "Not found"}), 404)
+
 
 if __name__ == '__main__':
     api_host = getenv("HBNB_API_HOST")
