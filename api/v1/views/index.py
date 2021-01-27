dbd7d92c8556c5e784aa56f23b00f00500b2c6b9
xioperez01 <angie.xioperez@gmail.com>
2021-01-27 16:38:21 +0000

    stats view added in index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,5 +6,17 @@ from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'])
 def status():
-    """app_views blueprint"""
+    """app_views status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """app_views stats"""
+    from models import storage
+    classes = {"amenity": "Amenity", "city": "City", "place": "Place",
+               "review": "Review", "state": "State", "user": "User"}
+    for key, value in classes.items():
+        classes[key] = storage.count(value)
+
+    return jsonify(classes)
