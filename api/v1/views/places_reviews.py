289a0f35e9646043279665840d3daec81e9ee4ea
xioperez01 <angie.xioperez@gmail.com>
2021-01-28 16:43:00 +0000

    new file:   api/v1/views/places_reviews.py modified:   api/v1/views/__init__.py modified:   api/v1/views/places.py

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,48 +1,50 @@
 #!/usr/bin/python3
-"""places view module"""
+"""reviews view module"""
 from flask import request, make_response, jsonify, abort
 from api.v1.views import app_views
 
 
-@app_views.route('/cities/<city_id>/places/', methods=['GET', 'POST'])
-@app_views.route('/cities/<city_id>/places', methods=['GET', 'POST'])
-def places(city_id):
-    """app_views places"""
+@app_views.route('/places/<place_id>/reviews/', methods=['GET', 'POST'])
+@app_views.route('/places/<place_id>/reviews', methods=['GET', 'POST'])
+def reviews(place_id):
+    """app_views reviews"""
     from models import storage
-    obj = storage.get("City", city_id)
+    obj = storage.get("Place", place_id)
     if obj is None:
         abort(404)
     if request.method == 'GET':
-        places_list = []
-        for place in storage.all("Place").values():
-            if place.city_id == city_id:
-                places_list.append(place.to_dict())
-        return jsonify(places_list)
+        reviews_list = []
+        for review in storage.all("Review").values():
+            if review.place_id == place_id:
+                reviews_list.append(review.to_dict())
+        return jsonify(reviews_list)
     else:
         content = request.get_json()
-        content["city_id"] = city_id
+        content["place_id"] = place_id
         if content is None:
             return make_response(jsonify("Not a JSON"), 400)
         elif 'name' not in content:
             return make_response(jsonify("Missing name"), 400)
+        elif 'text' not in content:
+            return make_response(jsonify("Missing text"), 400)
         elif 'user_id' not in content:
             return make_response(jsonify("Missing user_id"), 400)
         else:
             user_id = content['user_id']
             for user in storage.all("User").values():
                 if user.id == user_id:
-                    from models.place import Place
-                    new_obj = Place(**content)
+                    from models.review import Review
+                    new_obj = Review(**content)
                     new_obj.save()
                     return make_response(jsonify(new_obj.to_dict()), 201)
             abort(404)
 
 
-@app_views.route('/places/<place_id>', methods=['GET', 'PUT', 'DELETE'])
-def places_id(place_id):
-    """app_views places_id"""
+@app_views.route('/reviews/<review_id>', methods=['GET', 'PUT', 'DELETE'])
+def reviews_id(review_id):
+    """app_views reviews_id"""
     from models import storage
-    obj = storage.get("Place", place_id)
+    obj = storage.get("Review", review_id)
     if obj is None:
         abort(404)
 
@@ -60,8 +62,8 @@ def places_id(place_id):
             del content['updated_at']
         if 'user_id' in content:
             del content['user_id']
-        if 'city_id' in content:
-            del content['city_id']
+        if 'place_id' in content:
+            del content['place_id']
         for key, value in content.items():
             setattr(obj, key, value)
         obj.save()
